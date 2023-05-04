e4ac299787fc92417ac34780121b0bb1ffafdb9f
josh <makaze@gmail.com>
2023-05-04 13:21:52 -0500

    comparison operators

--- c/vm.c
+++ c/vm.c
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
+#include "value.h"
 
 VM vm;
 
@@ -84,6 +85,14 @@ InterperetResult run() {
             case OP_NIL:        push(NIL_VAL); break;
             case OP_TRUE:       push(BOOL_VAL(true)); break;
             case OP_FALSE:      push(BOOL_VAL(false)); break;
+            case OP_EQUAL: {
+                Value b = pop();
+                Value a = pop();
+                push(BOOL_VAL(valuesEqual(a, b)));
+                break;
+            }
+            case OP_GREATER:    BINARY_OP(BOOL_VAL, >); break;
+            case OP_LESS:       BINARY_OP(BOOL_VAL, <); break;
             case OP_ADD:        BINARY_OP(NUMBER_VAL, +); break;
             case OP_SUBTRACT:   BINARY_OP(NUMBER_VAL, -); break;
             case OP_MULTIPLY:   BINARY_OP(NUMBER_VAL, *); break;
