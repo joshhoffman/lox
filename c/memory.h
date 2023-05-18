17363be0147a0bd59252dfaf927b5d10742667e3
josh <makaze@gmail.com>
2023-05-18 12:26:35 -0500

    Mark-sweep garbage collector

--- c/memory.h
+++ c/memory.h
@@ -20,6 +20,9 @@
 #define FREE(type, pointer) (reallocate(pointer, sizeof(type), 0))
 
 void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void markValue(Value value);
+void markObject(Obj* object);
+void collectGarbage();
 void freeObjects();
 
 #endif
