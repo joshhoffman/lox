48f1bcc4be63c966ce871056f51b7ce929d67a84
josh <makaze@gmail.com>
2023-05-19 09:49:22 -0500

    Invoke methods

--- c/memory.c
+++ c/memory.c
@@ -69,6 +69,12 @@ static void blackenObject(Obj* object) {
         printf("\n");
     #endif
     switch (object->type) {
+        case OBJ_BOUND_METHOD: {
+            ObjBoundMethod* bound = (ObjBoundMethod*)object;
+            markValue(bound->receiver);
+            markObject((Obj*)bound->method);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
@@ -109,6 +115,10 @@ static void freeObject(Obj* object) {
         printf("%p free type %d\n", (void*)object, object->type);
     #endif
     switch(object->type) {
+        case OBJ_BOUND_METHOD: {
+            FREE(ObjBoundMethod, object);
+            break;
+        }
         case OBJ_CLASS: {
             ObjClass* klass = (ObjClass*)object;
             freeTable(&klass->methods);
